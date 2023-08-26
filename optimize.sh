b3dfa6049beb0d60c7d4c4e4b0d034ade86a7d1d
Chan <87248999+Chan5k@users.noreply.github.com>
2023-08-26 16:31:54 +0300

    Update optimize.sh

--- optimize.sh
+++ optimize.sh
@@ -34,18 +34,12 @@ display_welcome
 
 function check_new_version {
     remote_version=$(curl -s "https://raw.githubusercontent.com/Chan5k/beta-so/main/version.txt")
-    
-    # Extract numeric part of local and remote versions
-    local_version_numeric=$(echo "$VERSION" | grep -o '[0-9.]*')
-    remote_version_numeric=$(echo "$remote_version" | grep -o '[0-9.]*')
 
-    if [[ "$local_version_numeric" == "$remote_version_numeric" ]]; then
-        echo "You are using the latest version of the script."
-    elif [[ "$local_version_numeric" > "$remote_version_numeric" ]]; then
-        echo "You are using a newer version ($VERSION) than the remote version ($remote_version)."
-    else
+    if [[ "$remote_version" != "$VERSION" ]]; then
         echo "A new version ($remote_version) is available!"
         echo "Visit the repository for more information."
+    else
+        echo "You are using the latest version of the script."
     fi
 }
 
