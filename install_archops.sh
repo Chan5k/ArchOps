45ea345a946d82e6134731f61f2450af654dcc31
Chan5k <capatinamatei@protonmail.com>
2023-08-26 20:43:48 +0300

    Update install_archops.sh

--- install_archops.sh
+++ install_archops.sh
@@ -26,7 +26,6 @@ sudo pacman -Syu --noconfirm
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # Download ArchOps script and version.txt
-curl -o "$SCRIPT_DIR/archops.sh" -L "https://raw.githubusercontent.com/Chan5k/ArchOps/main/archops.sh"
 chmod +x "$SCRIPT_DIR/archops.sh"
 
 # Add cron job for background updates
