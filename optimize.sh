7a0c7eb5818e71ccb532a477c7833a66cd0fc16f
Chan <87248999+Chan5k@users.noreply.github.com>
2023-08-26 15:45:31 +0300

    Update optimize.sh

--- optimize.sh
+++ optimize.sh
@@ -12,44 +12,16 @@ function display_menu {
     echo "3. Show free disk space"
     echo "4. Show IP address"
     echo "5. Show DNS Servers in use"
-    echo "6. Change DNS Servers"
+    echo "6. Show System Information"
     echo "7. Exit"
 }
 
-function change_dns_servers {
-    echo "Select DNS Servers:"
-    echo "1. Google (8.8.8.8 and 8.8.4.4)"
-    echo "2. Quad9 (9.9.9.9 and 149.112.112.112)"
-    echo "3. Cloudflare (1.1.1.1 and 1.0.0.1)"
-
-    read -p "Enter your choice: " dns_choice
-
-    case $dns_choice in
-        1)
-            echo "Changing DNS servers to Google..."
-            sudo cp /etc/resolv.conf /etc/resolv.conf.backup
-            echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
-            echo "nameserver 8.8.4.4" | sudo tee -a /etc/resolv.conf > /dev/null
-            echo -e "${GREEN}DNS servers changed to Google successfully.${NC}"
-            ;;
-        2)
-            echo "Changing DNS servers to Quad9..."
-            sudo cp /etc/resolv.conf /etc/resolv.conf.backup
-            echo "nameserver 9.9.9.9" | sudo tee /etc/resolv.conf > /dev/null
-            echo "nameserver 149.112.112.112" | sudo tee -a /etc/resolv.conf > /dev/null
-            echo -e "${GREEN}DNS servers changed to Quad9 successfully.${NC}"
-            ;;
-        3)
-            echo "Changing DNS servers to Cloudflare..."
-            sudo cp /etc/resolv.conf /etc/resolv.conf.backup
-            echo "nameserver 1.1.1.1" | sudo tee /etc/resolv.conf > /dev/null
-            echo "nameserver 1.0.0.1" | sudo tee -a /etc/resolv.conf > /dev/null
-            echo -e "${GREEN}DNS servers changed to Cloudflare successfully.${NC}"
-            ;;
-        *)
-            echo -e "${RED}Invalid choice. No changes made to DNS servers.${NC}"
-            ;;
-    esac
+function show_system_info {
+    echo "System Information:"
+    echo "CPU: $(grep 'model name' /proc/cpuinfo | head -n 1 | awk -F ': ' '{print $2}')"
+    echo "Threads/Cores: $(grep 'cpu cores' /proc/cpuinfo | uniq | awk -F ': ' '{print $2}')c/$(grep 'siblings' /proc/cpuinfo | uniq | awk -F ': ' '{print $2}')t"
+    echo "RAM: $(free -h | grep 'Mem:' | awk '{print $2}') RAM"
+    echo "Disk Storage: $(df -h | grep '/dev/' | awk '{print $1 " = " $4 " FREE"}')"
 }
 
 while true; do
@@ -116,28 +88,25 @@ while true; do
             clear
             ;;
         3)
-            echo "Showing free disk space..."
             df -h
             sleep 1
             read -p "Press Enter to continue..."
             clear
             ;;
         4)
-            echo "Showing IP address..."
             ip addr show
             sleep 1
             read -p "Press Enter to continue..."
             clear
             ;;
         5)
-            echo "Showing DNS Servers in use..."
             cat /etc/resolv.conf
             sleep 1
             read -p "Press Enter to continue..."
             clear
             ;;
         6)
-            change_dns_servers
+            show_system_info
             sleep 1
             read -p "Press Enter to continue..."
             clear
