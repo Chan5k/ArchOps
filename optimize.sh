4e73451c9f5182da5a8f88c18aaa3b781d6cfd8e
Chan <87248999+Chan5k@users.noreply.github.com>
2023-08-26 15:28:45 +0300

    Update optimize.sh

--- optimize.sh
+++ optimize.sh
@@ -7,7 +7,7 @@ function display_menu {
     echo "3. Show free disk space"
     echo "4. Show IP address"
     echo "5. Show DNS Servers in use"
-    echo "6. Exit program."
+    echo "6. Exit."
 }
 
 while true; do
