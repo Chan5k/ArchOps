62bb67150ab51fb3e97afe8cd1cb01c974a27146
Chan <87248999+Chan5k@users.noreply.github.com>
2023-08-26 14:43:09 +0300

    Update optimize.sh

--- optimize.sh
+++ optimize.sh
@@ -17,11 +17,13 @@ while true; do
     case $choice in
         1)
             if command -v firewalld &>/dev/null; then
+                echo "Disabling firewall (firewalld)..."
                 sudo systemctl stop firewalld
                 echo "Firewall (firewalld) disabled successfully."
             else
                 read -p "Firewalld is not installed. Do you use UFW? (y/n): " ufw_choice
                 if [ "$ufw_choice" == "y" ]; then
+                    echo "Disabling UFW firewall..."
                     sudo ufw disable
                     echo "UFW firewall disabled successfully."
                 else
@@ -29,15 +31,18 @@ while true; do
                 fi
             fi
             sleep 1
+            read -p "Press Enter to continue..."
             clear
             ;;
         2)
             if command -v firewalld &>/dev/null; then
+                echo "Enabling firewall (firewalld)..."
                 sudo systemctl start firewalld
                 echo "Firewall (firewalld) enabled successfully."
             else
                 read -p "Firewalld is not installed. Do you use UFW? (y/n): " ufw_choice
                 if [ "$ufw_choice" == "y" ]; then
+                    echo "Enabling UFW firewall..."
                     sudo ufw enable
                     echo "UFW firewall enabled successfully."
                 else
@@ -45,21 +50,25 @@ while true; do
                 fi
             fi
             sleep 1
+            read -p "Press Enter to continue..."
             clear
             ;;
         3)
+            echo "Showing free disk space..."
             df -h
             sleep 1
             read -p "Press Enter to continue..."
             clear
             ;;
         4)
+            echo "Showing IP address..."
             ip addr show
             sleep 1
             read -p "Press Enter to continue..."
             clear
             ;;
         5)
+            echo "Showing DNS Servers in use..."
             cat /etc/resolv.conf
             sleep 1
             read -p "Press Enter to continue..."
@@ -72,6 +81,7 @@ while true; do
         *)
             echo "Invalid choice. Please select a valid option."
             sleep 1
+            read -p "Press Enter to continue..."
             clear
             ;;
     esac
